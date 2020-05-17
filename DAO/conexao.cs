c5259eb09f6e1f8b3a2dd6c5d54db641020a07c2
luiz.fifolato@outlook.com <luiz.fifolato@outlook.com>
2020-05-17 16:44:32 -0300

    ok

---
+++ DAO/conexao.cs
@@ -0,0 +1,55 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Login_sql.DAO
+{
+    public class conexao
+    {
+        #region Parãmetros De Conecxão
+
+        private static string Server = "localhost";
+        private static string DataBase = "web_session";
+        private static string User = "root";
+        private static string Passoword = "root";
+        private static string connectionString = $@"Server={Server};Database={DataBase};
+                                Uid={User};Pwd={Passoword};SslMode=none;charset=utf8;";
+
+
+        public static MySqlConnection connection = new MySqlConnection(connectionString);
+
+
+        #endregion
+
+        #region Conectar
+        public static void Conectar()
+        {
+
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+
+                connection.Open();
+
+            }
+
+        }
+        #endregion
+
+        #region Desconectar 
+
+        public static void Desconectar()
+        {
+
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As Informações Gerais sobre um assembly são controladas por meio do 
+// conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Login_sql")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Login_sql")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Configurar o ComVisible como false torna os tipos desse assembly invisíveis 
+// para componentes COM.  Se for necessário acessar um tipo nesse assembly a partir do 
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+[assembly: Guid("54514196-124b-4de3-bb6a-2576e8565cc2")]
+
+// As informações de versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou utilizar como padrão os Números de Revisão e da Versão 
+// usando o '*' como mostrado abaixo:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
